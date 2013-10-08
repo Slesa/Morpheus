8becf4a2edc05dd96cde52de207cd2c6f7a7aadf
Jörg Preiß <joerg.preiss@slesa.de>
2013-10-08 20:56:36 +0200

    Added editor project

---
+++ IniConfig/src/IniConfig.Editor/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace IniConfig.Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
