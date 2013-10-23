6e636d6115df08ba4bce59a72a3125f7a76e11af
J. Preiss <joerg.preiss@slesa.de>
2013-10-23 07:58:06 +0200

    Added console interface application for IniConfig

---
+++ IniConfig/src/IniConfig.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IniConfig.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("IniConfig console interface, version {0}");
+        }
+    }
+}
