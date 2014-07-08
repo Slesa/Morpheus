cd9e8993aab7e5b753e64ec5668fb41708611d99
Jörg Preiß <joerg.preiss@centigrade.de>
2014-07-08 14:40:34 +0200

    Added c't screen saver project

---
+++ Fractal/ext/LightsOut3D/LightsOut3D/App.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace LightsOut3D
+{
+  /// <summary>
+  /// Interaktionslogik für "App.xaml"
+  /// </summary>
+  public partial class App : Application
+  {
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+      TheApp = this;
+      if(!ParseArgs(e.Args))
+        this.Shutdown(-1);
+      if(Mode == RunMode.Config) {
+        MainWindow = new ConfigDialog();
+        if(ParentHandle != IntPtr.Zero) {
+          WindowInteropHelper wih = new WindowInteropHelper(MainWindow);
+          wih.Owner = ParentHandle;
+        }
+        MainWindow.ShowDialog();
+      }
+      else {
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+      }
+    }
+
+    private bool ParseArgs(string[] args)
+    {
+      Mode = RunMode.Config;
+      if(args.Length > 0) {
+        string arg = args[0].ToLower();
+        if(arg.StartsWith("/s")) {
+          Mode = RunMode.FullScreen;
+        }
+        else if(arg.StartsWith("/p")) {
+          Mode = RunMode.Preview;
+        }
+        else if(arg.StartsWith("/c")) {
+          Mode = RunMode.Config;
+        }
+        else {
+          MessageBox.Show("Unbekannte Kommandozeilenoption: " + arg, "LightsOut3D", MessageBoxButton.OK, MessageBoxImage.Error);
+          return false;
+        }
+        if(arg.Length > 3) {
+          arg = arg.Substring(3);
+        }
+        else if(args.Length > 1) {
+          arg = args[1];
+        }
+        Int64 handle;
+        if(Int64.TryParse(arg, out handle)) {
+          ParentHandle = new IntPtr(handle);
+        }
+        if(Mode == RunMode.Preview && ParentHandle == IntPtr.Zero) {
+          MessageBox.Show("Illegale Kommandozeilenoptionen: /p ohne Container-Handle", "LightsOut3D", MessageBoxButton.OK, MessageBoxImage.Error);
+          return false;
+        }
+      }
+      return true;
+    }
+
+    public enum RunMode
+    {
+      FullScreen,
+      Preview,
+      Config
+    }
+
+    public RunMode Mode
+    {
+      get;
+      private set;
+    }
+
+    public IntPtr ParentHandle
+    {
+      get;
+      private set;
+    }
+
+    public static App TheApp
+    {
+      get;
+      private set;
+    }
+  }
+}
---
+++ Fractal/ext/LightsOut3D/LightsOut3D/ConfigDialog.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LightsOut3D
+{
+  /// <summary>
+  /// Interaktionslogik für ConfigDialog.xaml
+  /// </summary>
+  public partial class ConfigDialog : Window
+  {
+    public ConfigDialog()
+    {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      bool allowPlay = Properties.Settings.Default.AllowInteractive;
+      cbAllowInteractive.IsChecked = allowPlay;
+      gbDifficulty.IsEnabled = allowPlay;
+      int difficulty = Properties.Settings.Default.Difficulty;
+      switch(difficulty) {
+        case 0:
+          rbEasy.IsChecked = true;
+          break;
+        case 1:
+          rbMedium.IsChecked = true;
+          break;
+        default:
+          rbHard.IsChecked = true;
+          break;
+      }
+    }
+
+    private void btnOk_Click(object sender, RoutedEventArgs e)
+    {
+      Properties.Settings.Default.AllowInteractive = cbAllowInteractive.IsChecked == true;
+      if(rbEasy.IsChecked == true)
+        Properties.Settings.Default.Difficulty = 0;
+      else if(rbMedium.IsChecked == true)
+        Properties.Settings.Default.Difficulty = 1;
+      else
+        Properties.Settings.Default.Difficulty = 2;
+      Properties.Settings.Default.Save();
+      this.Close();
+    }
+
+    private void btnCancel_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void cbAllowInteractive_Checked(object sender, RoutedEventArgs e)
+    {
+      gbDifficulty.IsEnabled = cbAllowInteractive.IsChecked == true;
+    }
+
+  }
+}
