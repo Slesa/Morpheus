79a3f57509dfd9b2bf3305febba37afdd9a2617f
Jörg Preiß <joerg.preiss@centigrade.de>
2014-10-16 18:35:56 +0200

    Euler 17

--- Euler/Euler.fs/Aufgaben/Aufgabe17.fs
+++ Euler/Euler.fs/Aufgaben/Aufgabe17.fs
@@ -13,18 +13,55 @@ type Loesung17 =
 
 type Aufgabe17() =
 
-    let einer = [| "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" |];
+    //               3      3       5       4       4       3       5        5       4                                          = 36
+    let einer = [| "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"; |];
+    //                 6         6           8           8          7          7          9             8           8           = 67
     let zehner = [| "eleven"; "twelve"; "thirteen"; "fourteen"; "fifteen"; "sixteen"; "seventeen"; "eighteen"; "nineteen" |];
+    //                    6         6         6         5        5         7          6         6
     let zwanziger = [| "twenty"; "thirty"; "fourty"; "fifty"; "sixty"; "seventy"; "eighty"; "ninety" |];
     let und = "and";
+    let zehn = "ten";
+    let hundert = "hundred";
+    let tausend = "thousand";
 
-    let EinerLength index =
-        einer.[index].Length
+    let LimitToIndex limit =
+        match limit with
+        | 0 -> 9
+        | _ -> limit
+
+    let Calculate array limit =
+        let index = LimitToIndex limit
+        let range = Array.sub array 0 index
+        let sum = Array.fold (fun acc (elem:string) -> acc + elem.Length) 0 range
+        sum
+
+    let EinerLength limit =
+        let sum = Calculate einer (limit % 10)
+        sum
+
+    let ZehnerLength limit =
+        let calcLimit = (limit % 100)
+        let sum = match calcLimit with
+        | x when x >= 20 -> Calculate zehner 0
+        | x when x > 10 && x < 20 -> Calculate zehner (limit % 10)
+        | _ -> 0
+        sum
+
+    let ZwanzigerLength limit =
+        let calcLimit = (limit % 100)
+        let range = calcLimit / 10
+        let sum = match calcLimit with
+        | x when x>=20 -> (range-2) * EinerLength(0) + Calculate zwanziger (range-1) // 0-9 und 11-20 schon drin
+        | _ -> 0
+        sum
 
     interface Loesung17 with
 
         member this.Calculate limit =
-            42
+            let sumEiner = EinerLength limit
+            let sumZehner = ZehnerLength limit
+            let sumZwanziger = ZwanzigerLength limit
+            sumEiner + sumZehner + sumZwanziger
 
     interface Aufgabe with
 
--- Euler/Euler.fs/Specs/Aufgabe17Specs.cs
+++ Euler/Euler.fs/Specs/Aufgabe17Specs.cs
@@ -4,13 +4,78 @@ using Machine.Specifications;
 namespace Specs
 {
     [Subject(typeof(Aufgabe17))]
+    public class When_solving_task17_with_limit_5
+    {
+        Establish context = () => _aufgabe = new Aufgabe17();
+
+        Because of = () => _result = _aufgabe.Calculate(5);
+
+        It should_calculate = () => _result.ShouldEqual(19);
+
+        static Loesung17 _aufgabe;
+        static int _result;
+    }
+
+    [Subject(typeof(Aufgabe17))]
     public class When_solving_task17_with_limit_10
     {
         Establish context = () => _aufgabe = new Aufgabe17();
 
         Because of = () => _result = _aufgabe.Calculate(10);
 
-        It should_calculate = () => _result.ShouldEqual(3);
+        It should_calculate = () => _result.ShouldEqual(39);
+
+        static Loesung17 _aufgabe;
+        static int _result;
+    }
+
+    [Subject(typeof(Aufgabe17))]
+    public class When_solving_task17_with_limit_11
+    {
+        Establish context = () => _aufgabe = new Aufgabe17();
+
+        Because of = () => _result = _aufgabe.Calculate(11);
+
+        It should_calculate = () => _result.ShouldEqual(45);
+
+        static Loesung17 _aufgabe;
+        static int _result;
+    }
+
+    [Subject(typeof(Aufgabe17))]
+    public class When_solving_task17_with_limit_15
+    {
+        Establish context = () => _aufgabe = new Aufgabe17();
+
+        Because of = () => _result = _aufgabe.Calculate(15);
+
+        It should_calculate = () => _result.ShouldEqual(39+35);
+
+        static Loesung17 _aufgabe;
+        static int _result;
+    }
+
+    [Subject(typeof(Aufgabe17))]
+    public class When_solving_task17_with_limit_20
+    {
+        Establish context = () => _aufgabe = new Aufgabe17();
+
+        Because of = () => _result = _aufgabe.Calculate(20);
+
+        It should_calculate = () => _result.ShouldEqual(39+67+6);
+
+        static Loesung17 _aufgabe;
+        static int _result;
+    }
+
+    [Subject(typeof(Aufgabe17))]
+    public class When_solving_task17_with_limit_21
+    {
+        Establish context = () => _aufgabe = new Aufgabe17();
+
+        Because of = () => _result = _aufgabe.Calculate(21);
+
+        It should_calculate = () => _result.ShouldEqual(39+67+6+3);
 
         static Loesung17 _aufgabe;
         static int _result;
