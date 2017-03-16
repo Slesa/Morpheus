df3fae2fa46c2f2ba38a1b8f1c598ccd4a33f787
J. Preiss <joerg.preiss@me.com>
2017-03-16 07:55:55 +0100

    Added UserRepository

--- Mikado/db/DbApplication/MainWindow.xaml.cs
+++ Mikado/db/DbApplication/MainWindow.xaml.cs
@@ -16,11 +16,10 @@ namespace DbApplication
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        string dataSource = "SQLiteDemo.db";
         private string _newUserName;
         private string _newUserDescr;
+        private UserRepository _userRepository;
 
-        private SQLiteConnection Connection { get; set; }
         public ObservableCollection<User> Users { get; private set; }
 
         public string NewUserName
@@ -41,98 +40,35 @@ namespace DbApplication
         public MainWindow()
         {
             Users = new ObservableCollection<User>();
+            _userRepository = new UserRepository();
             InitializeComponent();
         }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-
-            var createDemo = !File.Exists(dataSource);
-            Connection = OpenDatabase();
-            if (createDemo)
-                CreateDemoData();
+            _userRepository.CreateDemoDataIfNecessary();
             ReadUsers();
-        }
-
-        private SQLiteConnection OpenDatabase()
-        {
-            SQLiteConnection connection = new SQLiteConnection();
-
-            connection.ConnectionString = "Data Source=" + dataSource;
-            connection.Open();
-            return connection;
         }
 
         private void ReadUsers()
         {
             Users.Clear();
-            using (var command = new SQLiteCommand(Connection))
-            {
-                // Auslesen des zuletzt eingefügten Datensatzes.
-                command.CommandText = "SELECT id, name, descr FROM users";
-
-                using (var reader = command.ExecuteReader())
-                {
-
-                    while (reader.Read())
-                    {
-                        var id = reader.GetInt64(0);
-                        var user = new User() {Id = id, Name = reader[1].ToString(), Description = reader[2].ToString()};
-                        Users.Add(user);
-                    }
-
-                    // Beenden des Readers und Freigabe aller Ressourcen.
-                    reader.Close();
-                }
-            }
-            /* using (var command = new SQLiteCommand(Connection))
-            {
-                command.CommandText = "SELECT COUNT(id) FROM users";
-                command.CommandType = CommandType.Text;
-                UserCount = Convert.ToInt32(command.ExecuteScalar());
-            } */
+            var users = _userRepository.FindAll();
+            foreach(var user in users)
+                Users.Add(user);
         }
 
-        private void CreateDemoData()
-        {
-            using (SQLiteCommand command = new SQLiteCommand(Connection))
-            {
-                // Erstellen der Tabelle, sofern diese noch nicht existiert.
-                command.CommandText =
-                    "CREATE TABLE IF NOT EXISTS users ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(100) NOT NULL, descr VARCHAR(256));";
-                command.ExecuteNonQuery();
-
-                // Einfügen eines Test-Datensatzes.
-                command.CommandText = "INSERT INTO users (name) VALUES('Captain Picard')";
-                command.ExecuteNonQuery();
-                command.CommandText = "INSERT INTO users (name) VALUES('Lt Commander Riker')";
-                command.ExecuteNonQuery();
-                command.CommandText = "INSERT INTO users (name) VALUES('Lt Worf')";
-                command.ExecuteNonQuery();
-            }
-        }
 
         void AddUser(string name, string descr)
         {
-            using (SQLiteCommand command = new SQLiteCommand(Connection))
-            {
-                command.CommandText = string.Format("INSERT INTO users (name, descr) VALUES('{0}', '{1}')", name, descr);
-                command.ExecuteNonQuery();
-            }
+            _userRepository.Create(name, descr);
             ReadUsers();
         }
 
         void ChangeUser(long id, string name, string descr)
         {
-            using (SQLiteCommand command = new SQLiteCommand(Connection))
-            {
-                command.CommandText = "UPDATE users set name = :name, descr = :descr where id=:id";
-                command.Parameters.Add("name", DbType.String).Value = name;
-                command.Parameters.Add("descr", DbType.String).Value = descr;
-                command.Parameters.Add("id", DbType.Int64).Value = id;
-                command.ExecuteNonQuery();
-            }
+            _userRepository.Update(id, name, descr);
             ReadUsers();
         }
 
